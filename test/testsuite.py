a66d809338c007010cdac892a998c0be2ddbe527
Erik Troan <http://issues.rpath.com>
2008-12-12 09:29:46 -0500

    added license info everywhere

--- test/testsuite.py
+++ test/testsuite.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
-# -*- mode: python -*-
 #
-# Copyright (c) 2004-2008 rPath, Inc.
+# Copyright (c) 2008 rPath, Inc.
+#
+# This program is distributed under the terms of the MIT License as found 
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any waranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
 #
 import os
 import sys
