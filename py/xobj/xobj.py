ba36c7e8181cae50cd08d637b89753287662d6d2
Elliot Peele <https://issues.rpath.com/>
2009-01-05 23:47:24 -0500

    more accurate handling of list serialization

--- py/xobj/xobj.py
+++ py/xobj/xobj.py
@@ -144,12 +144,6 @@ class Document(XObject):
             element.text = xobj
             return element
 
-        if type(xobj) == list:
-            for item in xobj:
-                element = self.getElementTree(item, tag, rootElement=rootElement, nsmap=nsmap)
-                rootElement.append(element)
-            return rootElement
-
         tag = addns(tag)
 
         if hasattr(xobj, '_xobj'):
@@ -185,7 +179,10 @@ class Document(XObject):
                     attrs[key] = str(val)
                 else:
                     l = elements.setdefault(key, [])
-                    l.append(val)
+                    if type(val) == list:
+                        l.extend(val)
+                    else:
+                        l.append(val)
 
         orderedElements = []
 
